79321f9d04f08fd3914ee0e77a2e0ad9bb604cd7
tokisakiyuu <tokisakiyuu@qq.com>
2023-03-23 01:56:04 +0800

    修复跳转异常

--- pages/[key].tsx
+++ pages/[key].tsx
@@ -1,14 +1,23 @@
 import { GetServerSideProps } from 'next'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default function RedirectPage() {
   return null
 }
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const key = ctx.resolvedUrl.substring(1)
+  const result = await prisma.linkMap.findUnique({ where: { key } })
+  if (!result) return {
+    props: {},
+    notFound: true
+  }
   return {
     props: {},
     redirect: {
-      destination: 'https://baidu.com'
+      destination: result.link
     }
   }
 }
